1d8ed06adfbbcef2fb6ccacc7452fbf77a1428e1
강민지 <minjikang.dev@gmail.com>
2026-03-18 23:53:57 +0900

    반복문 while 실습 예제

---
+++ b1201_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n = 1;
+	while (n < 5) {
+		printf("====================\n");
+		n++;
+	}
+	int i = 0;
+	while (i < 5) {
+		printf("Hello world\n");
+		i++;
+	}
+	return 0;
+}
